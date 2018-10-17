87bb139e33cd643c3038adf3f74b3a29fadc5f18
Yashwanth Reddy <yashwanth@betterhalf.com>
2018-10-17 10:54:52 +0530

    appcenter removed in android and is working fine in android, ios getting errors

--- android/app/src/main/java/com/todoapp/MainApplication.java
+++ android/app/src/main/java/com/todoapp/MainApplication.java
@@ -3,9 +3,6 @@ package com.todoapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
-import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
-import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.microsoft.codepush.react.CodePush;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
@@ -45,9 +42,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
-            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
-            new AppCenterReactNativePackage(MainApplication.this),
             new RNGoogleSigninPackage(),
             new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
               new FBSDKPackage(mCallbackManager)
